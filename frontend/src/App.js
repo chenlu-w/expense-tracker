fc80e36ca1bbb78e755c93a62b75208b3b0f39cc
Chenlu Wang <karinawcl02@gmail.com>
2024-03-23 21:43:43 -0400

    continue frontendfinish background and orb

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import bg from "./img/bg.png";
+import { MainLayout } from "./styles/Layouts";
+import Orb from "./Components/Orb/Orb";
+
+function App() {
+  return (
+    <AppStyled bg={bg} className="App">
+      <Orb />
+
+      <MainLayout></MainLayout>
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${(props) => props.bg});
+  position: relative;
+`;
+export default App;
