3c007658e456afdcb6c55daf7e8e0bc1c19bb0b8
Chenlu Wang <karinawcl02@gmail.com>
2024-03-25 18:56:25 -0400

    errors that i dont know

--- frontend/src/Components/Form/Form.js
+++ frontend/src/Components/Form/Form.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useGlobalContext } from "../../context/globalContext";
 
 function Form() {
+  const { addIncome } = useGlobalContext();
   const [inputState, setInputState] = useState({
     title: "",
     amount: "",
@@ -17,8 +21,20 @@ function Form() {
     setError("");
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addIncome(inputState);
+    // setInputState({
+    //   title: "",
+    //   amount: "",
+    //   date: "",
+    //   category: "",
+    //   description: "",
+    // });
+  };
+
   return (
-    <FormStyled>
+    <FormStyled onSubmit={handleSubmit}>
       <div className="input-control">
         <input
           type="text"
@@ -26,6 +42,57 @@ function Form() {
           name={title}
           placeholder="Salary Title"
           onChange={handleInput("title")}
+        />
+      </div>
+      <div className="input-control">
+        <input
+          value={amount}
+          type="text"
+          name={"amount"}
+          placeholder={"Salary Amount"}
+          onChange={handleInput("amount")}
+        />
+      </div>
+      <div className="input-control">
+        <DatePicker
+          id="date"
+          placeholderText="Enter A Date"
+          selected={date}
+          dateFormat="dd/MM/yyyy"
+          onChange={(date) => {
+            setInputState({ ...inputState, date: date });
+          }}
+        />
+      </div>
+      <div className="selects input-control">
+        <select
+          required
+          value={category}
+          name="category"
+          id="category"
+          onChange={handleInput("category")}
+        >
+          <option value="" disabled>
+            Select Option
+          </option>
+          <option value="salary">Salary</option>
+          <option value="freelancing">Freelancing</option>
+          <option value="investments">Investiments</option>
+          <option value="stocks">Stocks</option>
+          <option value="bitcoin">Bitcoin</option>
+          <option value="bank">Bank Transfer</option>
+          <option value="youtube">Youtube</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <div className="submit-btn">
+        <Button
+          name={"Add Income"}
+          //   icon={plus}
+          //   bPad={".8rem 1.6rem"}
+          //   bRad={"30px"}
+          //   bg={"var(--color-accent"}
+          //   color={"#fff"}
         />
       </div>
     </FormStyled>
--- frontend/src/Components/Income/Income.js
+++ frontend/src/Components/Income/Income.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { useGlobalContext } from "../../context/globalContext";
 import { InnerLayout } from "../../styles/Layouts";
+import Form from "../Form/Form";
 
 function Income() {
   const { addIncome } = useGlobalContext();
@@ -10,7 +11,9 @@ function Income() {
       <InnerLayout>
         <h1>Income</h1>
         <div className="income-contnet">
-          <div className="form-container"></div>
+          <div className="form-container">
+            <Form />
+          </div>
         </div>
         <div className="incomes"></div>
       </InnerLayout>
